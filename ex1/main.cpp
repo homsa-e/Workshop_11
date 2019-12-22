7d4ac29936d018f2c54b7cf20e2524bc41bbc87c
homsa-e <55227510+homsa-e@users.noreply.github.com>
2019-12-22 22:04:05 +0300

    Update main.cpp

--- ex1/main.cpp
+++ ex1/main.cpp
@@ -11,90 +11,62 @@
 // Problem 2: delete zeroes from the vector without standard functions.
 // Problem 3: delete columns from the matrix which have zeroes
 
-// Please remind for the control work:
-// 1. Reading csv files. Converting string to integer/double std::stod, std::stoi
-// 2. set/map (e.g. constructors from vector, count, cycles, find over the set)
-// 3. deleting from containers
-// 4. converting data types (e.g. integer to double). 10 / 20 = 0
-
-
-
 void delete_zeroes_from_columns (std::vector<std::vector <int>> &matrix)
 {
+    size_t shift = 0, i, j, m = matrix[0].size(), n = matrix.size(), k = 0;
+    for (j = 0; j < m; j++){
+        k = 0;
+        for (i = 0; i < n; i++){
+            if (matrix[i][j] != 0)
+                k++;
+        }
+        if (k == n){
+            for (i = 0; i < n; i++){
+                matrix[i][shift] = matrix[i][j];
+            }
+            ++shift;
+        }
+    }
+    for (i = 0; i < n; i++)
+        matrix[i].resize(shift);
 }
 
-void delete_zeroes_from_vector ()
+void delete_zeroes_from_vector (std::vector<int> vec)
 {
-    std::vector<int> vec  = {1, 0, 2, 0, 3, 0, 4};
     std::vector<int>::iterator it = std::remove(vec.begin(), vec.end(), 0);
     vec.erase(it, vec.end());
-
     for (int elem : vec)
     {
         std::cout << elem << " ";
     }
 }
 
-void set_funcs ()
-{
-    std::vector<int> vec  = {1, 0, 2, 0, 3, 0, 4};
-
-    // Constructor
-    std::set<int> my_set (vec.begin (), vec.end ());
-
-    for (int element : my_set)
-    {
-        std::cout << element << "\n";
+void delete_zeroes_from_vector_without_st_f(std::vector<int> &vec){
+    size_t shift = 0, s = vec.size();
+    for (size_t i = 0; i < s; ++i) {
+        if (vec[i] != 0){
+            vec[shift] = vec[i];
+            ++shift;
+        }
     }
-
-    my_set.insert (10);
-
-    std::vector<int> vec2 = {10, 11, 12, 13};
-    for (int element : vec2)
-    {
-        my_set.insert(element);
-    }
-
-    if (my_set.find(10) != my_set.end ())
-    {
-        // we have element!
-    }
-
-    if (my_set.count(10) != 0 )
-    {
-
-    }
+    vec.resize(shift);
 }
 
-void map_funcs ()
-{
-    std::map<std::string, int> my_map;
-    int i = 10;
-    for (std::string name : {"a", "b", "c", "d"})
-    {
-        my_map[name] = i;
-        i += 10;
+int main(){
+    size_t n, m;
+    std::cin >> n >> m;
+    std::vector <std::vector <int> > v(n, std::vector <int> (m));
+    for (size_t i = 0; i < n; i++){
+        for (size_t j = 0; j < m;
+             j++)
+            std::cin >> v[i][j];
     }
-
-    for (const std::pair<std::string, int> &element : my_map)
-    {
-        std::cout << element.first << " " << element.second << "\n";
+    delete_zeroes_from_columns(v);
+    std::cout << std::endl;
+    for (size_t i = 0; i < v.size(); i++){
+        for (size_t j = 0; j < v[0].size();
+             j++)
+            std::cout << v[i][j] << ' ';
+        std::cout << std::endl;
     }
-
-    std::map<std::string, int>::iterator it = my_map.find ("a");
-    if (it != my_map.end ())
-    {
-        std::cout << it->first << " " << it->second; //key
-    }
-
 }
-
-int main ()
-{
-    return 0;
-}
-
-
-
-
-
